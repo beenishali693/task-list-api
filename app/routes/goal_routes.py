1e6cdffea8df028eb11bdefcacdd8e0dc3bf1fe3
Beenish <beenishali693@gmail.com>
2024-11-06 15:12:10 -0800

    wave 5 completed

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, abort, make_response, request
 from ..db import db
 from app.models.goal import Goal
+from app.routes.route_utilities import validate_model
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
@@ -28,3 +29,30 @@ def get_all_goals():
     goals_response = [goal.to_dict() for goal in goals]
     return goals_response
 
+
+@goals_bp.get("/<goal_id>")
+def get_one_task(goal_id):
+    goal = validate_model(Goal, goal_id)
+    return {"goal" : goal.to_dict()}
+
+@goals_bp.put("/<goal_id>")
+def update_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+    db.session.commit()
+
+    response = {"goal": goal.to_dict()}
+    return response, 200
+
+@goals_bp.delete("/<goal_id>")
+def delete_task(goal_id):
+    goal = validate_model(Goal, goal_id)
+    
+    db.session.delete(goal)
+    db.session.commit()
+
+    response = {'details': f'Goal {goal.id} "{goal.title}" successfully deleted'}
+    
+    return response, 200
